d69d8e8b29b0b6dff0052b247b658ab2c7af6766
PeepeePooPoo72270339 <onezeldafan@gmail.com>
2026-06-22 11:07:32 +1000

    Gun functionality
    Gun now can kill. Kill button removed and added a textbox.
    
    Gun also has outline

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class GunScript : MonoBehaviour
+{
+    public GameObject Self;
+    public GameObject Outline;
+    public InputAction MouseClick;
+    public LayerMask ButtonLayer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        MouseClick.Enable();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousepos = Mouse.current.position.ReadValue();
+        Ray mouseray = Camera.main.ScreenPointToRay(mousepos);
+
+        if (Physics.Raycast(mouseray, out RaycastHit hit, Mathf.Infinity, ButtonLayer))
+        {
+            if (hit.collider.gameObject == Self)
+            {
+                Outline.GetComponent<SpriteRenderer>().enabled = true;
+                if (MouseClick.WasPressedThisFrame())
+                {
+                    Self.GetComponent<ShootScript>().Kill();
+
+                }
+
+            }
+        }
+        else
+        {
+            Outline.GetComponent<SpriteRenderer>().enabled = false;
+
+
+        }
+        
+    }
+}
